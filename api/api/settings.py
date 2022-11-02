699ef6047e519446f91a3afcfd29cd9ec361619b
MohamedMuneerM <rehanamujeebh@gmail.com>
2022-11-02 18:35:42 +0530

    initial commit

--- api/api/settings.py
+++ api/api/settings.py
@@ -130,10 +130,10 @@ SWAGGER_SETTINGS = {
 
 }
 
-CORS_ALLOWED_ORIGINS = [
-	"http://localhost:8000",
-	"http://127.0.0.1:8000",
-]
+# CORS_ALLOWED_ORIGINS = [
+# 	"http://localhost:8000",
+# 	"http://127.0.0.1:8000",
+# ]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
